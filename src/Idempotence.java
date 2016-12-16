43c5d9eeed4cacb242bae44165818651edbda9de
elhagin <omar.elhagin@live.com>
2016-12-16 05:10:52 +0100

    final commit

--- src/Idempotence.java
+++ src/Idempotence.java
@@ -1,5 +1,5 @@
 import java.lang.reflect.Array;
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * Created by omarelhagin on 13/12/16.
@@ -47,22 +47,235 @@ public class Idempotence {
         return finalTerm;
     }
 
-    public static void unifyId(String term1Str, String term2Str, ArrayList<Character> idempotentFuncs) {
+    /**
+     *
+     * @param term1Str
+     * @param term2Str
+     * @param MGU
+     * @param idempotentFuncs
+     * @return
+     */
+    public static LinkedHashMap<Term, Term> unifyId(String term1Str, String term2Str, LinkedHashMap<Term, Term> MGU,
+                                              ArrayList<Character> idempotentFuncs) {
         Term term1 = parseFOLTerm(term1Str);
         Term term2 = parseFOLTerm(term2Str);
 
         term1.normalize(idempotentFuncs);
         term2.normalize(idempotentFuncs);
 
-        System.out.println(term1 + "\n" + term2);
-//        System.out.println(term1.term + ": " + term1.children.get(0).term + "\n"
-//                + term1.children.get(0).term + ": " + term1.children.get(0).children.get(0).term);
+        LinkedHashMap<Term, Term> result = unify(term1, term2, MGU, idempotentFuncs);
+        Iterator iter = result.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry<Term, Term> entry = (Map.Entry<Term, Term>) iter.next();
+            Iterator iter2 = result.entrySet().iterator();
+            while (iter2.hasNext()) {
+                if (entry.getKey().equals(new Term("x"))) {
+                    System.out.println();
+                }
+                Map.Entry<Term, Term> entry2 = (Map.Entry<Term, Term>) iter2.next();
+                if (entry.getKey().occursIn(entry2.getValue())) {
+                    entry2.getValue().replace(entry.getKey().term, entry.getValue());
+                }
+            }
+        }
+
+        return result;
+//        System.out.println(term1 + "\n" + term2);
+//        if (MGU.get(new Term("")) != null) {
+//            return null;
+//        }
+//        if (term1.equals(term2)) {
+//            return MGU;
+//        }
+
+        /**
+         * Constants are the first 13 letters in the English alphabet. Variables are the last 13 letters.
+         */
+//        if (term1.term.length() == 1) {
+//
+//            char term1Char = term1.term.toLowerCase().charAt(0);
+//            if (term1Char >= 'n' && term1Char <= 'z') { //if term1 is a variable
+////            return unifyVar(term1, term2, MGU);
+//            }
+//        }
+//
+//        if (term2.term.length() == 1) {
+//            char term2Char = term2.term.toLowerCase().charAt(0);
+//            if (term2Char >= 'n' && term2Char <= 'z') { //if term2 is a variable
+////            return unifyVar(term2, term1, MGU);
+//            }
+//        }
+//
+//        if (term1.children != null && term2.children != null) {
+////            return unifyId()
+//        }
+//        System.out.println(term1.children + "\n" + term2.children);
+//        if (!term1.term.equals(term2.term) && (term1.children == null || term2.children == null)) {
+//            if (term1.children == null && term2.children == null) {
+//                return term2 + "/" + term1;
+//            }
+//            if (term1.children != null) {
+//
+//            }
+//        }
+
+//        return null;
+    }
+
+    public static LinkedHashMap<Term, Term> unifyVar(Term variable, Object secondTerm, LinkedHashMap<Term, Term> MGU,
+                                                     ArrayList<Character> idempotentFuncs) {
+        if (MGU != null && MGU.containsKey(variable)) {
+            return unify(MGU.get(variable), secondTerm, MGU, idempotentFuncs);
+        }
+        if (MGU != null && secondTerm instanceof Term && MGU.containsKey(secondTerm)) {
+            return unify(variable, MGU.get(secondTerm), MGU, idempotentFuncs);
+        }
+        if (variable.occursIn(secondTerm)) {
+            LinkedHashMap<Term, Term> failMGU = new LinkedHashMap<>();
+            failMGU.put(new Term(""), new Term(""));
+            return failMGU;
+        }
+        if (MGU == null) {
+            LinkedHashMap<Term, Term> theMGU = new LinkedHashMap<>();
+            theMGU.put(variable, ((Term) secondTerm));
+            return theMGU;
+        }
+        else {
+            MGU.put(variable, ((Term) secondTerm));
+            Iterator iter = MGU.entrySet().iterator();
+            while (iter.hasNext()) {
+                Map.Entry<Term, Term> entry = (Map.Entry<Term, Term>) iter.next();
+                if (entry.getValue().equals(variable)) {
+                    entry.setValue(((Term) secondTerm));
+                }
+            }
+            return MGU;
+        }
+    }
+
+    public static LinkedHashMap<Term, Term> unify(Object firstTerm, Object secondTerm, LinkedHashMap<Term, Term> MGU,
+                                                  ArrayList<Character> idempotentFuncs) {
+        if (MGU != null && !MGU.isEmpty()) {
+            Iterator iter = MGU.entrySet().iterator();
+            while (iter.hasNext()) {
+                Map.Entry<Term, Term> entry = (Map.Entry<Term, Term>) iter.next();
+                if (firstTerm instanceof Term) {
+                    Term firstTermT = (Term) firstTerm;
+                    firstTermT.replace(entry.getKey().term, entry.getValue());
+                }
+                if (firstTerm instanceof ArrayList) {
+                    ArrayList<Term> firstTermL = (ArrayList) firstTerm;
+                    firstTermL.forEach(t -> t.replace(entry.getKey().term, entry.getValue()));
+                }
+                if (secondTerm instanceof Term) {
+                    Term secondTermT = (Term) secondTerm;
+                    secondTermT.replace(entry.getKey().term, entry.getValue());
+                }
+                if (secondTerm instanceof ArrayList) {
+                    ArrayList<Term> secondTermL = (ArrayList) secondTerm;
+                    secondTermL.forEach(t -> t.replace(entry.getKey().term, entry.getValue()));
+                }
+            }
+        }
+        if (firstTerm instanceof Term) {
+            Term firstTermT = (Term) firstTerm;
+            firstTermT.normalize(idempotentFuncs);
+        }
+        if (firstTerm instanceof ArrayList) {
+            ArrayList<Term> firstTermL = (ArrayList) firstTerm;
+            firstTermL.forEach(t -> t.normalize(idempotentFuncs));
+        }
+        if (secondTerm instanceof Term) {
+            Term secondTermT = (Term) secondTerm;
+            secondTermT.normalize(idempotentFuncs);
+        }
+        if (secondTerm instanceof ArrayList) {
+            ArrayList<Term> secondTermL = (ArrayList) secondTerm;
+            secondTermL.forEach(t -> t.normalize(idempotentFuncs));
+        }
+        if (MGU != null && MGU.get(new Term("")) != null) {//The empty string term represents failure
+            return MGU;
+        }
+
+        if (firstTerm.equals(secondTerm)) {
+            return MGU;
+        }
+
+        if (firstTerm instanceof Term && secondTerm instanceof Term) {
+            Term term1 = (Term) firstTerm;
+            Term term2 = (Term) secondTerm;
+
+            if (term1.children != null && term2.children != null) {
+                return unify(term1.children, term2.children,
+                        unify(term1.term, term2.term, MGU, idempotentFuncs), idempotentFuncs);
+            }
+        }
+
+        /**
+         * Constants are the first 13 letters in the English alphabet. Variables are the last 13 letters.
+         */
+        if (firstTerm instanceof Term && ((Term) firstTerm).term.length() == 1) {
+            Term term1 = (Term) firstTerm;
+            char term1Char = term1.term.toLowerCase().charAt(0);
+            if (term1Char >= 'n' && term1Char <= 'z') { //if term1 is a variable
+                return unifyVar(term1, secondTerm, MGU, idempotentFuncs);
+            }
+        }
+
+        if (secondTerm instanceof Term && ((Term) secondTerm).term.length() == 1) {
+            Term term2 = (Term) secondTerm;
+            char term2Char = term2.term.toLowerCase().charAt(0);
+            if (term2Char >= 'n' && term2Char <= 'z') { //if term2 is a variable
+                return unifyVar(term2, firstTerm, MGU, idempotentFuncs);
+            }
+        }
+        if (firstTerm instanceof ArrayList && secondTerm instanceof ArrayList) {
+            ArrayList<Term> firstTermCopy = cloneList((ArrayList) firstTerm);
+            ArrayList<Term> secondTermCopy = cloneList((ArrayList) secondTerm);
+            Object firstInT1 = firstTermCopy.remove(0);
+            Object firstInT2 = secondTermCopy.remove(0);
+            return unify(firstTermCopy, secondTermCopy,
+                    unify(firstInT1, firstInT2, MGU, idempotentFuncs), idempotentFuncs);
+        }
+
+        LinkedHashMap<Term, Term> failMGU = new LinkedHashMap<>();
+        failMGU.put(new Term(""), new Term(""));
+        return failMGU;
+    }
+
+    public static ArrayList<Term> cloneList(ArrayList<Term> a) {
+        ArrayList<Term> b = new ArrayList<>(a.size());
+        for (Term t : a) {
+            try {
+                b.add((Term) t.clone());
+            } catch (CloneNotSupportedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return b;
     }
 
     public static void main(String[] args) {
-        ArrayList<Character> s = new ArrayList<>();
-        s.add('P');
-        unifyId("P(P(P(x)))", "f(g(u),g(g(z)),z)",
-                s);
+        ArrayList<Character> fIdem = new ArrayList<>();
+        ArrayList<Character> gIdem = new ArrayList<>();
+        ArrayList<Character> noIdem = new ArrayList<>();
+        String[] firstTest = {"P(x,g(x),g(f(a)))", "P(f(u),v,v)"};
+        String[] secondTest = {"P(a,y,f(y))", "P(z,z,u)"};
+        String[] thirdTest = {"f(x,g(x),x)", "f(g(u),g(g(z)),z)"};
+        String[] fourthTest = {"f(x,g(x))", "f(g(x),x)"};
+        String normalizationTest = "g(s(x),e(y),f(y,f(y,y)),m(f(f(y))))";
+        fIdem.add('f');
+        gIdem.add('g');
+        System.out.println(unifyId(firstTest[0], firstTest[1], new LinkedHashMap<>(),
+                fIdem));
+        System.out.println(unifyId(secondTest[0], secondTest[1], new LinkedHashMap<>(),
+                noIdem));
+        System.out.println(unifyId(thirdTest[0], thirdTest[1], new LinkedHashMap<>(),
+                fIdem));
+        System.out.println(unifyId(thirdTest[0], thirdTest[1], new LinkedHashMap<>(),
+                gIdem));
+        System.out.println(unifyId(fourthTest[0], fourthTest[1], new LinkedHashMap<>(),
+                gIdem));
     }
 }
--- src/Term.java
+++ src/Term.java
@@ -18,16 +18,43 @@ public class Term {
     }
 
     public void normalize(ArrayList<Character> idempotentFuncs) {
-        if (idempotentFuncs.contains(term.charAt(0))) {
+//        if (children != null) {
+//            if (idempotentFuncs.contains(term.charAt(0))) {
+//                if (children.size() == 1 && children.get(0).term.equals(term)) {
+//                    children.add(children.get(0).children.get(0));
+//                    children.remove(0);
+//                    children.get(0).normalize(idempotentFuncs);
+//                }
+//                if (children.size() == 2 && children.get(0).term.equals(children.get(1).term)) {
+//                    term = children.get(0).term;
+//                    children = null;
+//                }
+//            }
+//        }
+        if (children != null) {
+//            if (children.size() > 2 || (children.size() == 2 && (children.get(0).children != null
+//                    || children.get(1).children != null))) {
+                children.forEach(term1 -> term1.normalize(idempotentFuncs));
+//            }
             if (children.size() == 1 && children.get(0).term.equals(term)) {
-                children.add(children.get(0).children.get(0));
-                children.remove(0);
-                children.get(0).normalize(idempotentFuncs);
+                if (idempotentFuncs.contains(term.charAt(0))) {
+                    children.add(children.get(0).children.get(0));
+                    children.remove(0);
+                    children.get(0).normalize(idempotentFuncs);
+                }
             }
-            if (children.size() == 2 && children.get(0).term.equals(children.get(1).term)) {
-                term = children.get(0).term;
-                children = null;
+            else {
+                if (children.size() == 2 && children.get(0).term.equals(children.get(1).term)) {
+                    if (idempotentFuncs.contains(term.charAt(0))) {
+                        term = children.get(0).term;
+                        children = null;
+                    }
+                }
+                else {
+
+                }
             }
+
         }
     }
 
@@ -37,7 +64,7 @@ public class Term {
             return term;
         }
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(term + ": [");
+        stringBuilder.append(term + "(");
         Iterator<Term> iter = children.iterator();
         while (iter.hasNext()) {
             Term t = iter.next();
@@ -46,7 +73,58 @@ public class Term {
             }
             stringBuilder.append(t.toString());
         }
-        stringBuilder.append("]");
+        stringBuilder.append(")");
         return stringBuilder.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || !(obj instanceof Term)) {
+            return false;
+        }
+        return term.equals(((Term) obj).term)
+                && (children == null ? ((Term) obj).children == null : children.equals(((Term) obj).children));
+    }
+
+    @Override
+    public int hashCode() {
+        int h = term.hashCode();
+        if (children != null) {
+            h += children.hashCode();
+        }
+        return h;
+    }
+
+    public boolean occursIn(Object secondTerm) {
+        Term term2 = (Term) secondTerm;
+        if (this.term.equals(term2.term)) {
+            return true;
+        }
+        if (term2.children == null) {
+            return false;
+        }
+
+        return term2.children.stream().map(t -> this.occursIn(t)).anyMatch(Boolean::booleanValue);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        if (children == null) {
+            return new Term(term, null);
+        }
+        else {
+            return new Term(term, (ArrayList) children.clone());
+        }
+    }
+
+    public void replace(String originalVal, Term replacement) {
+        if (term.equals(originalVal) && children == null) {
+            term = replacement.term;
+            children = replacement.children;
+        }
+
+        if (children != null) {
+            children.forEach(t -> t.replace(originalVal, replacement));
+        }
     }
 }
